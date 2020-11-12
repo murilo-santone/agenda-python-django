973f8650afe1bc557440aad90cd6cedf935d9aa7
Murilo <murilo.santone@hotmail.com>
2020-11-12 11:28:40 -0300

    Adicionado funções de alteração e exclusão de dados

--- core/models.py
+++ core/models.py
@@ -5,10 +5,11 @@ from datetime import date,time,datetime,timedelta
 # Create your models here.
 class Evento(models.Model):
     titulo = models.CharField(max_length=100) #informo o titulo e o max de caracteres
+    local_evento = models.CharField(max_length=50)  # campo para local do evento
     descricao = models.TextField(blank=True, null=True) #informo a descrição que pode ficar em branco e nulo
     data_evento = models.DateTimeField(verbose_name='data do evento') #informo para o usuário colocar o dia do evento
     data_criacao = models.DateTimeField(auto_now=True) #me retorna a hora que foi criado o evento.
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE) #mostrar usuários cadastrados import funçao django
 
     class Meta:
         db_table = 'evento' #informo o nome da tabela
@@ -18,3 +19,6 @@ class Evento(models.Model):
 
     def get_data_criacao(self):
         return self.data_evento.strftime('%d/%m/%Y %H:%M')
+
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
